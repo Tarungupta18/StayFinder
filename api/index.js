6a254cb9b15af7e7bba0552402550990a87a4954
tarun gupta <tkg21430@gmail.com>
2024-05-05 18:17:49 +0530

    second commit

---
+++ api/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User.js")
+require('dotenv').config();
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'tarun';
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin:'http://localhost:5173'
+}));
+
+// console.log(process.env.MONGO_URL);
+// mongoose.connect(process.env.MONGO_URL);
+
+main().then(() => {
+    console.log("connected to db");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+}
+
+app.get("/test", (req, res) => {
+    res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    console.log({ name });
+    try {
+        console.log("hello");
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        console.log("bye");
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json("bad");
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id }, jwtSecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token',token).json('pass ok');
+            });
+        } else {
+            res.status(422).json('pass not ok');
+        }
+    } else {
+        res.json('not found');
+    }
+})
+
+app.listen(4000, () => {
+    console.log("port is listening at 4000");
+});
---
+++ api/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: String,
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
