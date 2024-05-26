8819fa091437ab12b60766660234d59dbc4367c7
tarun gupta <tkg21430@gmail.com>
2024-05-26 10:57:13 +0530

    done

--- Client/src/PlaceGallery.jsx
+++ Client/src/PlaceGallery.jsx
@@ -1,59 +1,62 @@
 import {useState} from "react";
+import Image from "./Image.jsx";
 
-export default function PlaceGallery({ place }) {
+export default function PlaceGallery({place}) {
 
-    const [showAllPhotos, setShowAllPhotos] = useState(false);
+  const [showAllPhotos,setShowAllPhotos] = useState(false);
 
-    if (showAllPhotos) {
-        return (
-            <div className="absolute inset-0 bg-black text-white min-h-screen flex flex-col">
-                <div className="grid gap-4 p-8 justify-center bg-black">
-                    <div>
-                        <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
-                        <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 rounded-2xl py-2 px-4 shadow shadow-black bg-white text-black">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                            </svg>
-                            Close photos
-                        </button>
-                    </div>
-                    {place?.photos?.length > 0 && place.photos.map(photo => (
-                        <div className="flex justify-center">
-                            <img src={"http://localhost:4000/uploads/"+photo} alt="" />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
-
+  if (showAllPhotos) {
     return (
-        <div className="relative h-200 ">
-            <div className="grid grid-cols-3 gap-2 rounded-3xl overflow-hidden ">
-                <div className="col-span-2 row-span-2 ">
-                    {place.photos?.[0] && (
-                        <img onClick={() => setShowAllPhotos(true)} src={"http://localhost:4000/uploads/" + place.photos[0]} alt="" className="cursor-pointer object-cover w-full h-full " />
-                    )}
-                </div>
-                <div className="grid grid-rows-2 gap-2">
-                    {place.photos?.[1] && (
-                        <div className="row-span-1">
-                            <img onClick={() => setShowAllPhotos(true)} src={"http://localhost:4000/uploads/" + place.photos[1]} alt="" className="cursor-pointer object-cover w-full h-full" />
-                        </div>
-                    )}
-                    {place.photos?.[2] && (
-                        <div className="row-span-1 -mb-2 ">
-                            <img onClick={() => setShowAllPhotos(true)} src={"http://localhost:4000/uploads/" + place.photos[2]} alt="" className="cursor-pointer object-cover w-full h-full " />
-                        </div>
-                    )}
-                </div>
-            </div>
-            <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-gray-500">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75L7.409 10.591a2.25 2.25 0 013.182 0L15.75 15.75m-1.5-1.5L15.659 12.84a2.25 2.25 0 013.182 0l2.909 2.909M2.25 21h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12A1.5 1.5 0 003.75 21Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0Z" />
-                </svg>
-                Show more photos
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+            <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+              </svg>
+              Close photos
             </button>
+          </div>
+          {place?.photos?.length > 0 && place.photos.map(photo => (
+            <div>
+              <Image src={photo} alt=""/>
+            </div>
+          ))}
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div className="relative " >
+      <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden" >
+        <div>
+          {place.photos?.[0] && (
+            <div>
+              <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[0]} alt=""/>
+            </div>
+          )}
+        </div>
+        <div className="grid ">
+            <div >
+                {place.photos?.[1] && (
+                <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={place.photos[1]} alt=""/>
+                )}
+            </div>
+          <div className="overflow-hidden">
+            {place.photos?.[2] && (
+              <Image onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={place.photos[2]} alt=""/>
+            )}
+          </div>
+        </div>
+      </div>
+      <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow-md shadow-gray-500">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+          <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+        </svg>
+        Show more photos
+      </button>
+    </div>
+  );
 }
